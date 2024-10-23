288d804a54f51c636b459846e406a9df70cc165d
Damian Vicino <damian.vicino@datadoghq.com>
2024-10-23 11:59:18 -0400

    Initial wireframe of the project
    Major components are defined as files.
    Modules are declared.
    Tests run trivial 1 == 1 using pytest.
    setup.py config works in venvs

---
+++ generate_3rdparty-csv.py
@@ -0,0 +1,2 @@
+# This file is an entry point for the command line tool generate-3rd-party-csv.
+# Here we parse parameters and call the right metadata_collector and report_generator
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ospo-tools',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
+    ],
+    entry_points={
+        'console_scripts': [
+            'generate-3rd-party-csv=ospo_tools.generate_3rd_party_csv:main',
+        ],
+    },
+    author='Damian Vicino',
+    author_email='damian.vicino@datadoghq.com',
+    description='A set of tools to help with the OSPO activities and reporting',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/DataDog/ospo-tools',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
