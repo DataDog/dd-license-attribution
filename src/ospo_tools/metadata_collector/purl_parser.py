a2ca4c050fe33416908305b21bca88f36ed0045c
Damian Vicino <damian.vicino@datadoghq.com>
2024-11-05 13:56:43 -0500

    Improvements to PurlParser

--- src/ospo_tools/metadata_collector/purl_parser.py
+++ src/ospo_tools/metadata_collector/purl_parser.py
@@ -1,30 +1,41 @@
-"""This is a collection of tools to transform purls from different format and extract/validate parts of them"""
+"""This is a collection of tools to transform purls from different format and
+   extract/validate parts of them"""
 
 import re
 
 
 class PurlParser:
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
-    def get_github_owner_and_repo(self, purl):
+    def get_github_owner_and_repo(
+        self, purl: str | None
+    ) -> tuple[str | None, str | None]:
         if purl is None:
             return None, None
         # remove protocol from purl
         if "://" in purl:
             purl = purl.split("://")[1]
+        # remove version from purl if it is available
+        if "@" in purl:
+            purl = purl.split("@")[0]
         # Regular expression to match GitHub URLs and extract owner and repo
         match = re.match(r"^github\.com/([^/]+)/([^/]+)", purl)
         if match:
             return match.group(1), match.group(2)
         return None, None
 
-    def get_gitlab_owner_and_repo(self, purl):
+    def get_gitlab_owner_and_repo(
+        self, purl: str | None
+    ) -> tuple[str | None, str | None]:
         if purl is None:
             return None, None
         # remove protocol from purl
         if "://" in purl:
             purl = purl.split("://")[1]
+        # remove version from purl if it is available
+        if "@" in purl:
+            purl = purl.split("@")[0]
         # Regular expression to match GitLab URLs and extract owner and repo
         match = re.match(r"^gitlab\.com/([^/]+)/([^/]+)", purl)
         if match:
