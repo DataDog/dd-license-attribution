79dcca6cf89141ab964915e81ccaad3841371523
Damian Vicino <damian.vicino@datadoghq.com>
2024-11-07 12:00:01 -0500

    Adding test for github_repository collection strategy and bug fixes

--- src/ospo_tools/metadata_collector/strategies/github_repository_collection_strategy.py
+++ src/ospo_tools/metadata_collector/strategies/github_repository_collection_strategy.py
@@ -14,9 +14,6 @@ class GitHubRepositoryMetadataCollectionStrategy(MetadataCollectionStrategy):
     def augment_metadata(self, metadata: list[Metadata]) -> list[Metadata]:
         updated_metadata = []
         for package in metadata:
-            if not package.origin:
-                updated_metadata.append(package)
-                continue
             owner, repo = self.purl_parser.get_github_owner_and_repo(package.origin)
             if owner is None or repo is None:
                 updated_metadata.append(package)
@@ -25,11 +22,16 @@ class GitHubRepositoryMetadataCollectionStrategy(MetadataCollectionStrategy):
                 # get the repository information
                 status, repository = self.client.repos[owner][repo].get()
                 if status == 200:
-                    package.copyright = repository["owner"]["login"]
-                    if repository["license"]:
+                    if not package.copyright:
+                        package.copyright = repository["owner"]["login"]
+                    if repository["license"] and not package.license:
                         # get the license information
                         package.license = repository["license"].get("spdx_id", None)
                         if package.license == "NOASSERTION":
                             package.license = None
+                else:
+                    raise ValueError(
+                        f"Failed to get repository information for {owner}/{repo}"
+                    )
             updated_metadata.append(package)
         return updated_metadata
