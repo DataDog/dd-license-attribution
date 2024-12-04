c420f1c932efffe1f44c227c91e0cb9cf89aaf75
Ara Pulido <ara.pulido@datadoghq.com>
2024-12-04 16:53:31 +0100

    Search only under root and subfolder origin contains a subfolder

--- src/ospo_tools/metadata_collector/purl_parser.py
+++ src/ospo_tools/metadata_collector/purl_parser.py
@@ -8,11 +8,11 @@ class PurlParser:
     def __init__(self) -> None:
         pass
 
-    def get_github_owner_and_repo(
+    def get_github_owner_repo_path(
         self, purl: str | None
-    ) -> tuple[str | None, str | None]:
+    ) -> tuple[str | None, str | None, str | None]:
         if purl is None:
-            return None, None
+            return None, None, None
         # remove protocol from purl
         if "://" in purl:
             purl = purl.split("://")[1]
@@ -20,16 +20,16 @@ class PurlParser:
         if "@" in purl:
             purl = purl.split("@")[0]
         # Regular expression to match GitHub URLs and extract owner and repo
-        match = re.match(r"^github\.com/([^/]+)/([^/]+)", purl)
+        match = re.match(r"^github\.com/([^/]+)/([^/]+)(.*)", purl)
         if match:
-            return match.group(1), match.group(2)
-        return None, None
+            return match.group(1), match.group(2), match.group(3)
+        return None, None, None
 
-    def get_gitlab_owner_and_repo(
+    def get_gitlab_owner_repo_path(
         self, purl: str | None
-    ) -> tuple[str | None, str | None]:
+    ) -> tuple[str | None, str | None, str | None]:
         if purl is None:
-            return None, None
+            return None, None, None
         # remove protocol from purl
         if "://" in purl:
             purl = purl.split("://")[1]
@@ -37,7 +37,7 @@ class PurlParser:
         if "@" in purl:
             purl = purl.split("@")[0]
         # Regular expression to match GitLab URLs and extract owner and repo
-        match = re.match(r"^gitlab\.com/([^/]+)/([^/]+)", purl)
+        match = re.match(r"^gitlab\.com/([^/]+)/([^/]+)(.*)", purl)
         if match:
-            return match.group(1), match.group(2)
-        return None, None
+            return match.group(1), match.group(2), match.group(3)
+        return None, None, None
