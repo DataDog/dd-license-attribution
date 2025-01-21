331650ab9d12650d58028d350479c1113d4d1557
Ara Pulido <ara.pulido@datadoghq.com>
2025-01-21 11:52:10 +0100

    Add basic unit testing for CLI args

---
+++ tests/unit/test_get_licenses_copyrights.py
@@ -0,0 +1,58 @@
+import re
+
+from typer.testing import CliRunner
+
+from ospo_tools.cli.get_licenses_copyrights import app
+
+runner = CliRunner(mix_stderr=False)
+
+
+def test_basic_run() -> None:
+    result = runner.invoke(app, ["test", "--no-gh-auth"])
+    assert result.exit_code == 0
+
+
+def test_no_github_auth() -> None:
+    result = runner.invoke(app, ["test"])
+    assert result.exit_code == 2
+    assert "No Github token available" in result.stderr
+
+
+def test_github_auth_param() -> None:
+    result = runner.invoke(app, ["test", "--github-token=12345"])
+    assert result.exit_code == 0
+
+
+def test_github_auth_env() -> None:
+    result = runner.invoke(app, ["test"], env={"GITHUB_TOKEN": "12345"})
+    assert result.exit_code == 0
+
+
+def test_missing_package() -> None:
+    result = runner.invoke(app)
+    assert result.exit_code == 2
+    assert "Missing argument 'PACKAGE'." in result.stderr
+
+
+def test_cache_ttl_without_cache_dir() -> None:
+    result = runner.invoke(app, ["test", "--cache-ttl=10"], terminal_width=1000)
+    error = re.sub("\s+", " ", result.stderr)
+    assert result.exit_code == 2
+    assert (
+        "Invalid value for '--cache-ttl': Cannot specify --cache-ttl without │ │ --cache-dir"
+        in error
+    )
+
+
+def test_transitive_root_same_time() -> None:
+    result = runner.invoke(
+        app,
+        ["test", "--only-transitive-dependencies", "--only-root-project"],
+        terminal_width=20000,
+    )
+    error = re.sub("\s+", " ", result.stderr)
+    assert result.exit_code == 2
+    assert (
+        "Invalid value for '--only-root-project': Cannot specify both │ │ only-root-project and only-transitive-dependencies"
+        in error
+    )
