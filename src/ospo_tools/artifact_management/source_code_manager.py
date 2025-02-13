f49be5f7d82915790160128649bfa8980b9913bf
Damian Vicino <sdavtaker@users.noreply.github.com>
2025-02-13 17:09:23 -0500

    Extracting artifact manager class

--- src/ospo_tools/artifact_management/source_code_manager.py
+++ src/ospo_tools/artifact_management/source_code_manager.py
@@ -9,7 +9,8 @@ from ospo_tools.adaptors.os import (
 )
 from datetime import datetime
 import pytz
-from ospo_tools.adaptors.datetime import get_datetime_now
+
+from ospo_tools.artifact_management.artifact_manager import ArtifactManager
 
 
 @dataclass
@@ -18,15 +19,6 @@ class SourceCodeReference:
     branch: str
     local_root_path: str
     local_full_path: str
-
-
-def validate_cache_dir(local_cache_dir: str) -> bool:
-    for time_copy_str in list_dir(local_cache_dir):
-        try:
-            datetime.strptime(time_copy_str, "%Y%m%d_%H%M%SZ").replace(tzinfo=pytz.UTC)
-        except ValueError:
-            return False
-    return True
 
 
 def extract_ref(ref: str, url: str) -> str:
@@ -45,20 +37,7 @@ def extract_ref(ref: str, url: str) -> str:
     return ""
 
 
-class SourceCodeManager:
-    def __init__(self, local_cache_dir: str, local_cache_ttl: int = 86400) -> None:
-        self.local_cache_dir = local_cache_dir
-        self.local_cache_ttl = local_cache_ttl
-        self.setup_time = get_datetime_now()
-        self.timestamped_dir = self.setup_time.strftime("%Y%m%d_%H%M%SZ")
-        # validate the cache dir is a directory with only expected subdirectories
-        if not path_exists(local_cache_dir):
-            raise ValueError(f"Local cache directory {local_cache_dir} does not exist")
-        if not validate_cache_dir(local_cache_dir):
-            raise ValueError(
-                f"Local cache directory {local_cache_dir} has invalid subdirectory, are you sure it is a cache directory?"
-            )
-
+class SourceCodeManager(ArtifactManager):
     def get_code(
         self, resource_url: str, force_update: bool = False
     ) -> SourceCodeReference | None:
