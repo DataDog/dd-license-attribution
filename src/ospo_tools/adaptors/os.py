7b54119350024da279b945c1328a997cb8c66de4
Ara Pulido <ara.pulido@datadoghq.com>
2025-03-31 17:31:31 +0200

    Add copyright headers to all source files

--- src/ospo_tools/adaptors/os.py
+++ src/ospo_tools/adaptors/os.py
@@ -1,3 +1,8 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+#
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2024-present Datadog, Inc.
+
 """Here we collect a set of OS wrappers and adaptors to be easily replaced during testing and debugging."""
 
 from typing import Iterator
